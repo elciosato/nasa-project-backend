a93101690809dc389e16475a101e1030d2b51e60
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-09-18 16:14:04 +0200

    adjust listener

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
-
 require("dotenv").config();
 
 const app = require("./app");
@@ -9,12 +5,6 @@ const { loadLaunchesData } = require("./models/launches.model");
 const { loadPlanetsData } = require("./models/planets.model");
 const { mongoConnect } = require("./services/mongo");
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, "..", "cert", "server.key")),
-  cert: fs.readFileSync(path.join(__dirname, "..", "cert", "server.crt")),
-};
-
-const server = https.createServer(options, app);
 const PORT = process.env.PORT || 3333;
 
 async function startServer() {
@@ -22,7 +12,7 @@ async function startServer() {
   await loadPlanetsData();
   await loadLaunchesData();
 
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log("Server listening on port", PORT);
   });
 }
