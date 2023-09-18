f5cd7ed34a731fd0e2fe45bcdcdfa37df86333d5
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-09-18 14:47:30 +0200

    adjust http server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const http = require("http");
 
 const app = require("./app");
 const { loadLaunchesData } = require("./models/launches.model");
@@ -7,7 +7,7 @@ const { mongoConnect } = require("./services/mongo");
 
 require("dotenv").config();
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3333;
 
 async function startServer() {
